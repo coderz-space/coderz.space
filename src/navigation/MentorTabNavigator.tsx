7587d1945426d2f11b6a56d8dcc1e2c723334d69
shubhampandey45 <shubhampandey5410@gmail.com>
2026-03-26 00:15:56 +0530

    Adjusted bottom navigation

--- src/navigation/MentorTabNavigator.tsx
+++ src/navigation/MentorTabNavigator.tsx
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text, View, StyleSheet } from 'react-native';
 import { Colors, Typography, Spacing } from '../theme';
 import { MentorTabParamList } from '../types';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import MentorDashboardScreen from '../screens/mentor/DashboardScreen';
 import DoubtsScreen from '../screens/mentor/DoubtsScreen';
@@ -25,11 +26,16 @@ function TabIcon({ icon, focused, badge }: { icon: string; focused: boolean; bad
 }
 
 export default function MentorTabNavigator() {
+   const insets = useSafeAreaInsets(); 
   return (
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
-        tabBarStyle: tabStyles.bar,
+        tabBarStyle: {
+          ...tabStyles.bar,
+          height: 64 + insets.bottom,        // ADD insets.bottom
+          paddingBottom: insets.bottom + 4,  // ADD insets.bottom
+        },
         tabBarShowLabel: true,
         tabBarLabelStyle: tabStyles.label,
         tabBarActiveTintColor: Colors.primary,
@@ -76,10 +82,7 @@ const tabStyles = StyleSheet.create({
   bar: {
     backgroundColor: Colors.surface,
     borderTopWidth: 1,
-    borderTopColor: Colors.surfaceBorder,
-    height: 64,
-    paddingBottom: Spacing.sm,
-    paddingTop: Spacing.sm,
+    borderTopColor: Colors.surfaceBorder
   },
   label: {
     ...Typography.labelSmall,
